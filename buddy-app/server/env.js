2ffd0e82ecd33c3ad9f27a7c44444bc716788884
Ben Xiong <xiongbenjamin@gmail.com>
2024-01-30 11:31:37 -0600

    add notes

--- buddy-app/server/env.js
+++ buddy-app/server/env.js
@@ -1,15 +1,4 @@
-import { getUser } from './controllers/appController'
-import { useAuthStore } from '../store/store'
-
-export const  Username = () => {
-    const { username } = useAuthStore( state => state.auth )
-    const emailuser = getUser(username)
-    return emailuser
-}
-
-console.log (Username())
-
 module.exports = {
-    EMAIL: Username(),
+    EMAIL: "xiongbenjamin@gmail.com",
     PASSWORD:'irco uxeo ftcq bhmc'
 }
